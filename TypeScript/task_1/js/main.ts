c98374fb5550ee632ebe1fbf4ccd58e08d911c46
Priam <priamdmy@gmail.com>
2026-01-14 16:51:04 +0100

    [update] added printTeacher fn as per task 3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,6 +11,14 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  printTeacher: (firstName: string, lastName: string) => string;
+}
+
+function printTeacher(firstName: string, lastName: string) {
+  console.log(`${firstName.substring(0, 1)}. ${lastName}`);
+}
+
 const teacher1: Teacher = {
   firstName: 'Bob',
   fullTimeEmployee: true,
@@ -19,21 +27,14 @@ const teacher1: Teacher = {
   canPaint: true,
 };
 
-const teacher2: Teacher = {
-  firstName: 'Fred',
-  fullTimeEmployee: false,
-  lastName: 'Rogers',
-  location: 'Pennsylvania',
-  wearsCardigan: true,
-};
-
 const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
+  firstName: 'Anders',
+  lastName: 'Hejlsberg',
+  location: 'Denmark',
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
 
 console.log(director1);
-console.log(teacher1, teacher2);
+console.log(teacher1);
+printTeacher("Bob", "Ross");
