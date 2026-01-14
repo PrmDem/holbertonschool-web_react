63959892f1bbfbfc1ad8b663e2212ac61901233f
Priam <priamdmy@gmail.com>
2026-01-14 16:27:52 +0100

    [update] implements Directors interface as per task 2

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,6 +7,10 @@ interface Teacher {
   [index: string]: any;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 const teacher1: Teacher = {
   firstName: 'Bob',
   fullTimeEmployee: true,
@@ -23,4 +27,13 @@ const teacher2: Teacher = {
   wearsCardigan: true,
 };
 
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
 console.log(teacher1, teacher2);
